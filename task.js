c23f678ccf042917b2079c9009b30db5624f1010
hank <hank8451@gmail.com>
2020-08-19 15:27:07 +0800

    task2 complete

--- task.js
+++ task.js
@@ -1,13 +1,30 @@
 let formSubmit = document.forms[1]
+myStorage = window.localStorage
+// let sampleData=[]
+let nowStorage
+
+// console.log(myStorage)
+// var records
 formSubmit.addEventListener("submit",(e) => {
   e.preventDefault()
-  
-  console.log({
+  // console.log({
+  //   date: formSubmit["date"].value,
+  //   category: formSubmit["category"].value,
+  //   description: formSubmit["description"].value,
+  //   amount: formSubmit["amount"].value
+  // })
+  let oddData = JSON.parse(localStorage.getItem('records')) || []
+
+  addData={
     date: formSubmit["date"].value,
     category: formSubmit["category"].value,
     description: formSubmit["description"].value,
     amount: formSubmit["amount"].value
-  })
+  }
+  oddData.push(addData) 
+
+  localStorage.setItem('records', JSON.stringify(oddData))
+  // console.log(JSON.stringify(oddData))
   formSubmit["date"].value = ""
   formSubmit["category"].value = ""
   formSubmit["description"].value = ""
